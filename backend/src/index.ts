c6f5df16e31179d8d4a082d048c7cd5cab2bbb8a
Abhishek <abhishek.mahato@innovatetech.co>
2026-03-27 12:04:38 +0545

    feat: setup backend

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+// src/index.ts
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from TypeScript + Express!");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
